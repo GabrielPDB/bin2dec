67726c92b1b26b9bda9f9657fab7ddf852bf2965
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2021-08-25 06:19:49 -0300

    challenge done

--- scripts.js
+++ scripts.js
@@ -4,17 +4,29 @@ const decimal_input = document.querySelector('#decimal_input')
 
 function convert() {
   const value = binary_input.value
-  console.log(value)
+  let valid = true
 
   if (value === '' || Array.from(value.toString()).length > 8) {
+    valid = false
+  }
+
+  Array.from(value.toString()).forEach(char => {
+    if (char !== '0' && char !== '1') valid = false
+  })
+
+  if (valid) {
+    decimal_input.value = parseInt(value, 2)
+    return true
+  } else {
     inputAlert()
     return false
   }
-
-  decimal_input.value = parseInt(value, 2)
-  return true
 }
 
 function inputAlert() {
   alert('Please insert a valid binary number!')
 }
+
+document.addEventListener('keydown', event => {
+  if (event.key == 'Enter') convert()
+})
